af899e833f8b896b2becb3fe062f6f50dbb6daf3
Afsana-20 <145790122+Afsana-20@users.noreply.github.com>
2026-07-14 14:40:26 +0530

    Create 724-FindPivotIndex

---
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -0,0 +1,19 @@
+// Last updated: 7/14/2026, 2:43:28 PM
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0,left=0,right=0;
+        for (int i:nums) {
+            sum=sum+i;
+        }
+        for (int i=0;i<nums.length;i++) {
+            right=sum-left-nums[i];
+            if (left==right) {
+                return i; 
+            }
+            left=left+nums[i]; 
+        }
+        return -1; 
+    }
+
+}
+    
