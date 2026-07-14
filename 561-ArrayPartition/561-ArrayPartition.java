740f5ef5de0364318b786ea593181440465b1dde
Afsana-20 <145790122+Afsana-20@users.noreply.github.com>
2026-07-14 14:40:28 +0530

    Create 561-ArrayPartition

---
+++ 561-ArrayPartition/561-ArrayPartition.java
@@ -0,0 +1,12 @@
+// Last updated: 7/14/2026, 2:43:29 PM
+import java.util.Arrays;
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int sum=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i+=2){
+            sum+=nums[i];
+        }
+        return sum;
+    }
+}
